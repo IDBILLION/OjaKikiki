eb549c386db9172254b4bc2b0f03cebdc5a90a6b
IDBILLION <153001200+IDBILLION@users.noreply.github.com>
2026-03-27 23:39:59 +0100

    Remove Profile component implementation

--- src/ProfileCard/Profile.tsx
+++ src/ProfileCard/Profile.tsx
@@ -1,17 +1,1 @@
-import { useState } from "react"
 
-function Profile () {
-    const initialValue = {name: "", title: "", bio: ""};
-   const [details, setDetails] = useState(initialValue);
-   
-    return (
-        <>
-        <div className="card">
-<h2>{name}</h2>
-<p className="card-title">{title}</p>
-<p>{bio}</p>
-        </div>
-        <Profile name="Idris" title="Frontend Developer" bio="I like it here" />
-        </>
-    )
-} export default Profile;
